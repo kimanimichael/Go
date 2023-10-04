f5f41635a0a2fb42a7c0844c79082a47f9c63208
MK <mikalkimmani@gmail.com>
2023-10-04 05:51:21 +0300

    (master): For loop with ommitted final condition

--- Loops/main.go
+++ Loops/main.go
@@ -15,7 +15,22 @@ func bulkSend(numMessages  int) float64 {
 	return totalCost
 }
 
+func maxMessages(thresh float64) int {
+	baseCost := 1
+	additionalCost := 0.01
+	var totalCost float64
+	//Condition ommitted in for loop
+	for i := 0; ; i++ {
+		// cost := float64(baseCost * (i+1)) + (0.01* float64(i))
+		totalCost += float64(baseCost) + (additionalCost)* float64(i)
+		if totalCost > thresh {
+			return i
+		}
+	}
+}
+
 func main () {
 	fmt.Printf("Total cost %.2f \n", bulkSend(10))
+	fmt.Printf("Total messages %d \n", maxMessages(10))
 
 }
