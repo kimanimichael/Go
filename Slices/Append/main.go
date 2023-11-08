f04ffeec9d81b995e33f1d7ed9301112330367eb
MK <mikalkimmani@gmail.com>
2023-11-08 21:36:57 +0300

    (master): Appending to slices

--- Slices/Append/main.go
+++ Slices/Append/main.go
@@ -14,10 +14,44 @@ func createMessageSlice () []string {
 	return messageSlice
 }
 
+type cost struct {
+	day int
+	value float64
+}
+
+
+
+func getCostsByDay(costs []cost) []float64 {
+	costByDay := []float64{}
+
+	for i:=0; i<len(costs); i++ {
+		cost := costs[i]
+		for cost.day >= len(costByDay) {
+			costByDay = append(costByDay, 0.0)
+			
+		}
+		
+		costByDay[cost.day] += cost.value
+
+	}
+	return costByDay
+}
+
 func main() {
 	messageSlice := createMessageSlice()
 	fmt.Println(messageSlice)
 	/* test append function */
 	messageSlice = append(messageSlice, "MK")
 	fmt.Println(messageSlice)
+	
+	weekCost:= []cost{
+		{0, 4.0},
+		{1, 2.1},
+		{1, 3.1},
+		{5, 2.5},
+	}
+
+	fmt.Println(len(weekCost))
+
+	fmt.Println(getCostsByDay(weekCost))
 }
